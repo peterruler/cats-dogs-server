f1b0dc94781e1b93bb196e11e0d76601991ad063
Peter Strössler <7starch@gmail.com>
2023-12-17 12:47:28 +0100

    jpeg only

--- app.js
+++ app.js
@@ -39,14 +39,14 @@ const storage = multer.diskStorage({
 });
 
 function checkFileType(file, cb) {
-  const filetypes = /jpeg|jpg|png|gif/;
+  const filetypes = /jpeg|jpg/;
   const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
   const mimetype = filetypes.test(file.mimetype);
 
   if (mimetype && extname) {
     return cb(null, true);
   } else {
-    cb("Please upload images only");
+    cb("Please upload jpeg images only");
   }
 }
 
