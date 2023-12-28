d88773dc408ec57d1aca810fe16535f47e930d5c
Peter Strössler <7starch@gmail.com>
2023-12-28 16:44:16 +0100

    add of necessary command

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-# conda activate tensorflow && node app.js
+# conda activate tensorflow && node app.js # for local testing
+# then to a CTRL + c to kill process
 # npm install forever -g
 forever start app.js
