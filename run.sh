290b69918dcbff6f8eda592dd8dd25686914afbb
Peter Strössler <7starch@gmail.com>
2023-12-29 10:58:55 +0100

    fixed typo

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # conda activate tensorflow && node app.js # for local testing
-# then to a CTRL + c to kill process
+# then do a CTRL + c to kill process
 # npm install forever -g
 forever start app.js
